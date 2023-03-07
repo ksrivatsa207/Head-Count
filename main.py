fd748d942b4dbd97149251f915ca71b150ebfebd
ksrivatsa207 <ksrivatsa207@gmail.com>
2023-03-07 19:12:28 +0530

    Commit

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def run_cmd(command):
             print("Process interrupted")
             sys.exit(1)
 #for detection
-cmd="python ./yolov5/detect.py --weights ./yolov5/runs/train/exp37/weights/best.pt --classes 0 --project ./yolov5/runs/detect/ --iou-thres 0.25 --source ./sdf.jpg --imgsz 800 --line-thickness 1 --hide-conf"
+cmd="python ./yolo/detect.py --weights ./yolo/runs/train/exp37/weights/best.pt --classes 0 --project ./yolo/runs/detect/ --iou-thres 0.25 --source ./sdf.jpg --imgsz 800 --line-thickness 1 --hide-conf"
 #for training
-# cmd="python yolov5/train.py --data coco128.yaml --weights '' --cfg yolov5s.yaml --img 640 --project ./yolov5/runs/train/clg_ds --epochs 150 --batch 4 --device cpu"  # from scratch
-# cmd="python yolov5/export.py --weights ./mod.pth --include tflite --img 416"
+# cmd="python yolo/train.py --data coco128.yaml --weights '' --cfg yolov5s.yaml --img 640 --project ./yolo/runs/train/clg_ds --epochs 150 --batch 4 --device cpu"  # from scratch
+# cmd="python yolo/export.py --weights ./mod.pth --include tflite --img 416"
 run_cmd(cmd)
